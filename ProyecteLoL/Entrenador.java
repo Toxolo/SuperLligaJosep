0976312ca7265104d92ecb7d721f5e85c3fb4fcc
Gabriel DAM <estudiosgabriel1975@gmail.com>
2025-03-12 08:41:42 +0100

    Algoridtme

---
+++ ProyecteLoL/Entrenador.java
@@ -0,0 +1,28 @@
+public class Entrenador extends Participant{
+    private String nom;
+    private String cognom;
+    private String nickname;
+    private int edat;
+
+    public Entrenador(String nom, String cognom, String nickname, int edat) {
+        super(nom, cognom, nickname, edat);
+    }
+
+  
+
+    // public String getNom() {
+    //     return nom;
+    // }
+
+    // public String getCognom() {
+    //     return cognom;
+    // }
+
+    // public String getNickname() {
+    //     return nickname;
+    // }
+
+    // public int getEdat() {
+    //     return edat;
+    // }
+}
---
+++ ProyecteLoL/algoritmeversioJosep.java
@@ -0,0 +1,51 @@
+import java.util.Random;
+
+public class algoritmeversioJosep {
+    public static void main(String[] args) {
+        int min = 40000;  // Límite inferior
+        int max = 90000;  // Límite superior        
+        Random random = new Random();
+        int numeroAleatorio = random.nextInt(max - min + 1) + min;
+        int numeroAleatorio2 = random.nextInt(max - min + 1) + min;
+        int EquipBlau = numeroAleatorio;
+        int EquipRoig = numeroAleatorio2;
+        int Diferencia;
+        int posibilita = 50;
+        if (EquipBlau > EquipRoig){
+            Diferencia = (EquipBlau - EquipRoig)/1000;
+            int blau = posibilita+Diferencia;
+            int roig = posibilita-Diferencia;
+            System.out.println("El equip blau te un "+ (blau)+"%");
+            System.out.println("El equip roig te un "+ (roig)+"%");
+            guanyador( blau, roig);
+        }
+        else if (EquipBlau < EquipRoig){
+            Diferencia = (EquipRoig - EquipBlau)/1000;
+            int blau = posibilita+Diferencia;
+            int roig = posibilita-Diferencia;
+            System.out.println("El equip blau te un "+ (blau)+"%");
+            System.out.println("El equip roig te un "+ (roig)+"%");
+            guanyador( blau, roig);
+        }
+        else {
+            int blau = 50;
+            int roig = 50;
+            System.out.println("El equip blau te un "+ (posibilita)+"%");
+            System.out.println("El equip roig te un "+ (posibilita)+"%");
+            guanyador( blau, roig);
+        }
+        
+    }
+    public static void guanyador( int blau, int roig) {
+        int min = 1;  // Límite inferior
+        int max = 100;  // Límite superior  
+        Random random = new Random();      
+        int guanyadorf = random.nextInt(max - min + 1) + min;
+        if (0<=guanyadorf && guanyadorf<=blau){
+            System.out.println("Ha guanyat el equip Blau");
+        }
+        else {
+            System.out.println("Ha guanyat el equip Roig");
+        }
+    }
+}
